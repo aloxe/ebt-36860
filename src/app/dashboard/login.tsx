095ae4c98411127d8352b21c8b7d79abd456079f
aloxe <github@blog.re>
2023-08-10 01:17:29 +0200

    helper for login

--- src/app/dashboard/login.tsx
+++ src/app/dashboard/login.tsx
@@ -1,71 +1,31 @@
 'use client'
 
-// import { FormEventHandler } from "react";
+import { useState } from "react";
+import { fetchWrapper } from "../helpers/fetch-wrapper";
 
 export function Login() {
 
-	// TODO handle state and errors from https://jasonwatmore.com/next-js-13-mongodb-user-registration-and-login-tutorial-with-example-app
-	const formState = { isSubmitting: false };
+    // TODO handle state and errors from https://jasonwatmore.com/next-js-13-mongodb-user-registration-and-login-tutorial-with-example-app
+    const formState = { isSubmitting: false };
 
-	async function checkuser(username: string, password: string) {
-		const details: { [key: string]: string } = {
-        'my_email': username,
-        'my_password': password
-    } as const;
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        // Preventing the page from reloading
+        event.preventDefault();
 
+        const login = (event.target as HTMLInputElement).getElementsByTagName("input")[0].value
+        const password = (event.target as HTMLInputElement).getElementsByTagName("input")[1].value
+        console.log("click", login);
+        const params = {
+            'my_email': login,
+            'my_password': password
+        };
+        const user:any = await fetchWrapper.post('/api/eurobilltracker/?m=login&v=2&PHPSESSID=123456789', params);
 
-		var formBody: String[] = [];
-		for (var property in details) {
-			var propertyst = property.toString()
-      var encodedKey = encodeURIComponent(property);
-      var encodedValue = encodeURIComponent(details[property]);
-      formBody.push(encodedKey + "=" + encodedValue);
-    }
-    let formBodyTxt = formBody.join("&");
-
-		// NOT TODO: use https://stackoverflow.com/questions/65058598/nextjs-cors-issue to avoid CORS
-		// now using : https://reacthustle.com/blog/nextjs-send-post-request-to-external-api
-		const response = await fetch('/api/eurobilltracker/?m=login&v=2&PHPSESSID=123456789', {
-			method: 'POST',
-			// mode: 'no-cors',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-			},
-			body: formBodyTxt
-		})
-		.then(function (response) {
-			// handle opaque response
-			console.log("login response ", response);
-			response.text().then(function (data) {
-				console.log('data:' + data);
-			});
-		})
-		.catch(function (err) {
-			console.log('Fetch Error :-S', err);
-		});
-
-		// const user = await response.text();
-
-
-		// return user;
-		// const cityResponse = await fetch(`https://api.eurobilltracker.com/?m=mycities&v=1&PHPSESSID=${user.sessionid}`)
-		// console.log("cities response", cityResponse);
-		// const cities = await cityResponse.json();
-		// console.log("cities data", cities);
-
-}
-
-	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-		// Preventing the page from reloading
-		event.preventDefault();
-
-		const login = (event.target as HTMLInputElement).getElementsByTagName("input")[0].value
-		const password = (event.target as HTMLInputElement).getElementsByTagName("input")[1].value
-		console.log("click", login);
-
-		const user = await checkuser(login, password)
-		console.log("login data", user);
-
+        if (user) {
+            console.log("we have user", user);
+        } else {
+            console.log("not a user");
+        }
 	}
 
 	return (
---
+++ src/app/helpers/fetch-wrapper.tsx
@@ -0,0 +1,45 @@
+// import getConfig from 'next/config';
+
+// import { userService } from 'services';
+
+// const { publicRuntimeConfig } = getConfig();
+
+export const fetchWrapper = {
+	get: request('GET'),
+	post: request('POST')
+};
+
+function request(method:string) {
+    return async (url:string, body:any) => {
+        const requestOptions = {
+            method,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            },
+            body: body ? getRequestBody(body) : "" 
+        };
+        await fetch(url, requestOptions)
+            .then(function (response) {
+                // handle opaque response
+                response.json().then(function (data) {
+                    console.log('data:', data);
+                    // todo set cookie
+                    // setUser(data);
+                    return data;
+                });
+            })
+            .catch(function (err) {
+                console.log('Fetch Error :-S', err);
+            });
+    }
+}
+
+function getRequestBody(body:any) {
+    var bodyArray: String[] = [];
+    for (var property in body) {
+        var encodedKey = encodeURIComponent(property);
+        var encodedValue = encodeURIComponent(body[property]);
+        bodyArray.push(encodedKey + "=" + encodedValue);
+    }
+    return bodyArray.join("&");
+}
