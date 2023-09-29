b6236fbcacb8ccb24879efed0944e2db4c731bc7
aloxe <github@blog.re>
2023-09-29 23:41:48 +0200

    tri par départements

--- src/app/stats/[id]/page.tsx
+++ src/app/stats/[id]/page.tsx
@@ -1,57 +1,88 @@
-  import prisma from "@/lib/prisma";
+  import { fetchComplete } from "@/helpers/cityutils";
+import { getUserVisited } from "@/helpers/dbutils";
 
-const UserData = async ({ params }: { params: { id: string } }) => {
+interface commune {
+    "code": string;
+    "nom": string;
+    "surface": number;
+    "population": number;
+    "codesPostaux": string[];
+    "centre": { type: 'Point', coordinates: Array },
+    "contour": { type: 'Polygon', coordinates: Array },
+    "zone": 'metro',
+    "departement": { code: string, nom: string; },
+    "region": { code: string, nom: string; }
+  }
+
+  const UserData = async ({ params }: { params: { id: string } }) => {
   console.log("c parti");
   console.log(params.id);
 
-  const response = await prisma.visited.findUnique({
-    where: {
-        user_id: params.id,
-    },
-  });
-  const visited = JSON.parse(response?.content || "")
-  // console.log("res", visited?.communes);
+  const visited = await getUserVisited(params.id);
 
-const fetchAllCommunes = async () => {
-  // curl 'https://geo.api.gouv.fr/communes?nom=Versailles&fields=code,nom,surface,population,codesPostaux,codeDepartement,codeRegion,siren,codeEpci,epci,departement,region,centre,contour,zone'
-  const response = await fetch(
-      `https://geo.api.gouv.fr/communes?fields=code,nom`
-    )
-  const results = await response.json()
-  return results;
-}
-
-const allCommunes = await fetchAllCommunes();
-const AllEtalabCommunes = require('@etalab/decoupage-administratif/data/communes.json')
-const skimEtalab = () => {
-  // const communes = AllEtalabCommunes.filter( item => item.type !== "commune-deleguee")
-  const communes = AllEtalabCommunes.filter( item => item.type === "commune-actuelle")
-  // console.log(communes);
-  return communes;
-}
-const AllEtalabCommunesFilltered = skimEtalab()
-    console.log("allAPICommunes ←←←←←←←←←←←←←←← length " + allCommunes.length);
-    console.log("AllEtalabCommunes <=<=<=<=<=<=<= length " + AllEtalabCommunes.length);
-    // console.log("AllEtalabCommunes ←←←←←←←←←←←←←←← " + AllEtalabCommunesFilltered.length);
+var visitedCommunes: commune[] = await Promise.all(visited.communes.map(async (code: string): Promise<commune> => {
+    let visitedCommune = await fetchComplete(code);
+    return visitedCommune;
+}));
+ 
+visitedCommunes.sort( (a, b) => {
+    if (a.codesPostaux[0] < b.codesPostaux[0]) {
+        return -1;
+    }
+    if (a.codesPostaux[0] > b.codesPostaux[0]) {
+        return 1;
+    }
+    return 0;
+});
 
 
-const getName = (code:string) => {
-  const commune = allCommunes.find( item => item.code === code)
-  // console.log(communes);
-  return commune?.nom;
-}
+// console.log(visitedCommunes, "…..…..……..…..……..…..……..…..……..…..……..…..……..…..……..…..……..…..……..…..……..…..……..…..…");
+
+
+// visited.communes.map( async (code: string) => {
+//   console.log(code, "←--");
+//   const plop = await fetchComplete(code);
+//   console.log(plop.nom);
+  
+//   console.log("… . . … . … . . …");
+  
+  
+// });
+
+
+//   const allCommunes = await fetchAllCommunes();
+// const AllEtalabCommunes = require('@etalab/decoupage-administratif/data/communes.json')
+// const skimEtalab = () => {
+//   // const communes = AllEtalabCommunes.filter( item => item.type !== "commune-deleguee")
+//   const communes = AllEtalabCommunes.filter( item => item.type === "commune-actuelle")
+//   // console.log(communes);
+//   return communes;
+// }
+// const AllEtalabCommunesFilltered = skimEtalab()
+//     console.log("allAPICommunes ←←←←←←←←←←←←←←← length " + allCommunes.length);
+//     console.log("AllEtalabCommunes <=<=<=<=<=<=<= length " + AllEtalabCommunes.length);
+//     // console.log("AllEtalabCommunes ←←←←←←←←←←←←←←← " + AllEtalabCommunesFilltered.length);
+
+
+// const getName = (code:string) => {
+//   const commune = allCommunes.find( item => item.code === code)
+//   // console.log(communes);
+//   return commune?.nom;
+// }
 
 
 
-const getObject = (code:string) => {
-  const communes = allCommunes.filter( item => item.code === code)
-  // console.log(communes);
-  return communes || "→ "+code+" ←";
-}
+// const getObject = (code:string) => {
+//   const communes = allCommunes.filter( item => item.code === code)
+//   // console.log(communes);
+//   return communes || "→ "+code+" ←";
+// }
 
-const communeBizarre = getObject("22264")
-console.log("cheflieu de communeBizarre", communeBizarre);
+// const communeBizarre = getObject("22264")
+// console.log("cheflieu de communeBizarre", communeBizarre);
 
+
+// console.log(visitedCommunes);
 
 // const communeBizare = getObject("22264")
 // console.log("communeBizare", communeBizare);
@@ -96,6 +127,9 @@ console.log("cheflieu de communeBizarre", communeBizarre);
 
       // console.log("allVisited", allVisited);
       
+          const departements = require('@etalab/decoupage-administratif/data/departements.json')
+          console.log(departements);
+          
   return (
   <>
     <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
@@ -103,10 +137,46 @@ console.log("cheflieu de communeBizarre", communeBizarre);
       {visited?.username}
     </h1>
      <div className="bg-white rounded-lg border border-blue-200 text-left text-blue-900 p-4 m-5">
-      {/* {allCommuneNames.map((name, index) => <span key={index}>{name}</span>)} */}
-      {visited?.communes.map((code, index) => <span key={index}>{getName(code) || <b>→ {code} ←</b>} </span>)}
 
-     </div>
+
+        <table className="min-w-full text-left text-sm font-light">
+          <thead className="border-b font-medium dark:border-neutral-500">
+            <tr>
+              <th colSpan={3} className="whitespace-nowrap px-6 py-4">user title</th>
+            </tr>
+          </thead>
+          <tbody>
+            {departements.map((departement: any)  => (
+              <>
+              <tr key={departement.code}>
+                  <th colSpan={3} className="whitespace-nowrap px-6 py-4">{departement.nom} ({departement.code})</th>
+                  <th colSpan={1} className="whitespace-nowrap px-6 py-4">{visitedCommunes.filter(function(commune: commune){
+                  return commune.departement.code === departement.code }).length}</th>
+              </tr>
+              {visitedCommunes.map( commune => ( 
+                commune.departement.code === departement.code &&
+                <tr className="border-b dark:border-neutral-500" key={commune.code}>
+                  <td className="whitespace-nowrap px-6 py-4">
+                    {commune.codesPostaux[0]}
+                  {!!commune.codesPostaux[1] && "…"}
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4">{commune.nom}</td>
+                  <td className="whitespace-nowrap px-6 py-4">
+                    {(commune.surface/100).toFixed(2)} km²<br />
+                    {commune.population} hab.
+                  </td>
+                </tr>
+              ))}
+              </>
+          ))}
+          </tbody>
+        </table>
+    </div>
+
+    <div className="bg-white rounded-lg border border-blue-200 text-left text-blue-900 p-4 m-5">
+      {/* {allCommuneNames.map((name, index) => <span key={index}>{name}</span>)} */}
+      {/* {visited?.communes.map((code, index) => <span key={index}>{getName(code) || <b>→ {code} ←</b>} </span>)} */}
+    </div>
     </div>
   </>
   )
