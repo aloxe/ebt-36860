4f2e70b5a71a4f367ad71d4d56d4b95aed803c0f
aloxe <github@blog.re>
2023-10-03 08:32:39 +0200

    create list communes par départements

--- src/components/common/jeuCommunes.tsx
+++ src/components/common/jeuCommunes.tsx
@@ -4,107 +4,73 @@ async function JeuCommunes({user, visited}: {user: publicUser, visited: visited}
 
   const allCommunesWithDomTom = await fetchAllComplete();
   // we remove oversea municipalities
-    // @ts-ignore
+  // @ts-ignore
   const allCommunes = allCommunesWithDomTom.filter(el => el.zone === "metro")
-
   const firstPourcent = visited.communes.length * 100 / allCommunes.length;
 
   // @ts-ignore
   const allCommunesPop: number[] = allCommunes.map(el => el.population || 0)
   const allPop = allCommunesPop.reduce((a, b) => a + b)
-    // @ts-ignore
+  // @ts-ignore
   const visitedPop = visited.communes.map(code => allCommunes.find(el => el.code === code).population )
   const visitedPopulation = visitedPop.reduce((a, b) => a + b)
   const secondPourcent = visitedPopulation * 100 / allPop;
 
-    // @ts-ignore
+  // @ts-ignore
   const allCommunesSurf: number[] = allCommunes.map(el => el.surface)
   const allSurface = allCommunesSurf.reduce((a, b) => a + b) / 100
-    // @ts-ignore
+  // @ts-ignore
   const visitedSurf = visited.communes.map(code => allCommunes.find(el => el.code === code).surface )
   const visitedSurface = (visitedSurf.reduce((a, b) => a + b) /100)
   const thirdPourcent = visitedSurface * 100 / allSurface;
 
   const inseeAltitude = require('@/data/correspondance-code-insee-altitude-2013.json')
-
   // @ts-ignore
 const visitedNumbers = allCommunes.filter(c => visited.communes.includes(c.code) ).map(c => ({
   code: c.code, 
   nom: c.nom, 
   population: c.population,
   surface: c.surface / 100,
-      // @ts-ignore
+  // @ts-ignore
   altitude: parseInt(inseeAltitude.find(el => el.code === c.code).altitude),
 }))
 
-    // @ts-ignore
+  // @ts-ignore
 const maxPop = visitedNumbers.reduce((prev, current) => {
     return (prev && prev.population > current.population) ? prev : current
 })
-    // @ts-ignore
+  // @ts-ignore
 const minPop = visitedNumbers.reduce((prev, current) => {
     return (prev && prev.population < current.population) ? prev : current
 })
-    // @ts-ignore
+  // @ts-ignore
 const maxSurf = visitedNumbers.reduce((prev, current) => {
     return (prev && prev.surface > current.surface) ? prev : current
 })
-    // @ts-ignore
+  // @ts-ignore
 const minSurf = visitedNumbers.reduce((prev, current) => {
     return (prev && prev.surface < current.surface) ? prev : current
 })
-    // @ts-ignore
+  // @ts-ignore
 const maxAlt = visitedNumbers.reduce((prev, current) => {
     return (prev && prev.altitude > current.altitude) ? prev : current
 })
-    // @ts-ignore
+  // @ts-ignore
 const minAlt = visitedNumbers.reduce((prev, current) => {
     return (prev && prev.altitude < current.altitude) ? prev : current
 })
-    // @ts-ignore
+  // @ts-ignore
   const visitedAlt = visitedNumbers.filter(c => c.altitude)
-    // @ts-ignore
-      const visitedAltMoyenne = (visitedAlt.reduce((a, b) => a + b.altitude, 0)) / visitedAlt.length
-
-  
-  // console.log(visitedNumbers);
-  
-  // visited.communes.map(code => {
-  //   code
-  //   allCommunes.find(el => el.code === code).nom,
-  //   allCommunes.find(el => el.code === code).population,
-  //   allCommunes.find(el => el.code === code).surface
-  //   // inseeAltitude.find(el => el.code === code).altitude || undefined
-  // })
-
-
-  
-// const max = visited.communes.reduce((prev, current) => {
-//     return (prev && prev.population > current.population) ? prev : current
-// })
-
-  // const visitedSurf = visited.visitedCities.map(el => el.surface)
-  // const visitedSurface = allCommunesSurf.reduce((a, b) => a + b)
-
-
-
-  // console.log("visitedPop", visitedPop, visitedPopulation);
-
-// console.log(allCommunesSurf);
-
-  // const allCommunesPop = allCommunes.map(el => el.population)
-
+  // @ts-ignore
+  const visitedAltMoyenne = (visitedAlt.reduce((a, b) => a + b.altitude, 0)) / visitedAlt.length
 
   return (
     <>
       <div className="bg-white rounded-lg border border-blue-200 text-left text-black p-4 m-5">
         <div className="flex justify-between">
           <h2 className="mb-3 text-lg font-semibold">
-            ▤ {user.user_name} score in 36860 ▥
+            {user.user_name} score in ▤ 36860 communes ▥
           </h2>
-          {/* <div className="text-right text-stone-400 text-sm">
-            last updated: {date}
-          </div> */}
         </div>
         <div className="text-left text-lg font-bold mb-4">
           <span className="text-blue-600">{visited.communes.length}</span> communes soit <span className="text-blue-600">{firstPourcent.toFixed(2)}%</span> du nombre de communes<br />
