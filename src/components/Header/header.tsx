4f6597aaa854519d5bc4f58ff3e692eb8a89c7c9
aloxe <github@blog.re>
2023-10-03 20:49:43 +0200

    try to fix build

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { Login } from './login';
 
-export default function Header() {
+export function Header() {
   return (
     <div className='grid grid-cols-4 gap-4 top-0 p-0 m-0 h-12 bg-sky-200 border-b-2 border-black fixed w-full z-1001'>
       <div className='col-span-3 p-2 text-black md:text-xl font-semibold'>
