1bf668624a37c1284d8b4c6a5c5f5a6bd4958380
aloxe <github@blog.re>
2023-10-04 23:27:14 +0200

    split list of communes by dept

--- src/components/common/listeCommunes.tsx
+++ src/components/common/listeCommunes.tsx
@@ -1,4 +1,5 @@
 import { fetchComplete } from "@/helpers/cityutils";
+import ListeCommunesDepartement from "./listeCommunesDepartement";
 
 async function ListeCommunes({user, visited}: {user: publicUser, visited: visited}) {
 
@@ -22,28 +23,11 @@ async function ListeCommunes({user, visited}: {user: publicUser, visited: visite
           </thead>
           <tbody>
             {departements.map((departement: any)  => (
-              <>
-              <tr key={departement.code} className={visitedCommunes.filter(function(commune: commune){
-                  return commune?.departement?.code === departement.code }).length < 1 ? "bg-sky-100 opacity-20" : "bg-sky-200"}>
-                  <th colSpan={3} className="whitespace-nowrap px-6 py-4">{departement.nom} ({departement.code})</th>
-                  <th colSpan={1} className="whitespace-nowrap px-6 py-4">{visitedCommunes.filter(function(commune: commune){
-                  return commune?.departement?.code === departement.code }).length}</th>
-              </tr>
-              {visitedCommunes.map( commune => ( 
-                commune?.departement?.code === departement.code &&
-                <tr className="border-b dark:border-neutral-500" key={commune.code}>
-                  <td className="whitespace-nowrap px-6 py-4">
-                    {commune?.codesPostaux[0]}
-                  {!!commune?.codesPostaux[1] && "…"}
-                  </td>
-                  <td className="whitespace-nowrap px-6 py-4">{commune.nom}</td>
-                  <td className="whitespace-nowrap px-6 py-4">
-                    {(commune?.surface || 0 / 100).toFixed(2)} km²<br />
-                    {commune?.population} hab.
-                  </td>
-                </tr>
-              ))}
-              </>
+              <ListeCommunesDepartement 
+                key={departement.code}
+                visitedCommunes={visitedCommunes.filter( commune => ( 
+                commune?.departement?.code === departement.code))}
+                departement={departement} />
           ))}
           </tbody>
         </table>
