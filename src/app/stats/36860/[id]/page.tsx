f0a14494472f754cca3e85895734c3d521b16c46
aloxe <github@blog.re>
2023-10-04 22:54:55 +0200

    try to split pages

--- src/app/stats/36860/[id]/page.tsx
+++ src/app/stats/36860/[id]/page.tsx
@@ -1,4 +1,4 @@
-import Summary from "@/components/common/summary";
+import JeuCommunes from "@/components/common/jeuCommunes";
 import { getUserVisited } from "@/helpers/dbutils";
 import { getPublicUser } from "@/helpers/ebtutils";
 
@@ -32,7 +32,7 @@ console.log("visited done" + visited.date);
 
     return (
       <>
-      <Summary user={publicUser} visited={visited}/>
+      {/* <Summary user={publicUser} visited={visited}/>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <menu>
           36860 score ⋅
@@ -41,9 +41,9 @@ console.log("visited done" + visited.date);
           tour de France ⋅
           jeu des préfectures
         </menu>
-      </div>
-      {/* <ListeCommunes user={publicUser} visited={visited}/>
-      <JeuCommunes user={publicUser} visited={visited}/> */}
+      </div> */}
+      {/* <ListeCommunes user={publicUser} visited={visited}/> */}
+      <JeuCommunes user={publicUser} visited={visited}/>
       </>
     )
 }
