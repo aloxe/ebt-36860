c2bab51e501b3f6afa42de575bd598ce61d16a81
aloxe <github@blog.re>
2023-11-12 00:15:21 +0100

    translate FAQ

--- src/app/[lang]/faq/page.tsx
+++ src/app/[lang]/faq/page.tsx
@@ -1,58 +1,67 @@
 import TitleButton from "@/components/common/titleButton";
 import Link from "next/link";
+import { useTranslation } from '@/i18n'
 
-const Faq = async () => {
+const Faq = async ({ params: { lang } }: { params: { lang: string}}) => {
+  /* eslint-disable react-hooks/rules-of-hooks */
+  const { t } = await useTranslation(lang, 'faq')
 
   return (
     <>
       <div className="relative flex place-items-center mb-2 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1>Travel in France, collect euros and map your your trips</h1>
+        <h1>{t("moto")}</h1>
       </div>
 
       <div className="group bg-white rounded-lg border border-blue-200 text-left  p-2 m-2 sm:p-4 sm:m-4">
-        <h2>What is Eurobilltracker?</h2>
+        <h2>{t("what-is-ebt")}</h2>
         <p className="mb-4">
-        Eurobilltracker is a website that allows you to record euro banknote serial numbers. If someone else finds the same banknote you will be able to follow where your banknotes go after you spend them.
+        {t("what-is-ebt-res")}
         </p>
 
-        <h2>What is the purpose of Eurobilltracker?</h2>
+        <h2>{t("what-is-purpose")}</h2>
         <p className="mb-4">
-        Tracking banknotes is useless, meaningless and futile, therefore this is essencial. Thouthands are tracking banknotes all over the world, wherever they go. Eurobilltracker helps them connect and if lucky, they will perform a <i>hit</i>, when someone find a note that was already tracked on Eurobilltracker.
+        {t("what-is-purpose-res1")}
         </p>
         <p className="mb-4">
-           Want to try by yourself? <a href="https://en.eurobilltracker.com/?referer=31378">Just register</a> and start track your euros now!
-        </p>
-
-        <h2>What is the site ▤ 36680 ▥ about?</h2>
-        <p className="mb-4">
-        ▤ 36680 ▥ Helps you track all locations you visited based on the euros you tracked on Eurobilltracker.
+        {t("what-is-purpose-res2")}
         </p>
         <p className="mb-4">
-        When tracking euros while travelling, it is interesting to pinpoint where you&apos;ve been but it is not always easy to remember all places. It is particularly difficult in France where the number of municipalities is huge. In 2000 France counted no less than 36680 municipalities. This gargantuan number gave a name to this site.
-        </p>
-        <p className="mb-4">
-        When you log in with your Eurobilltracker credentials, it we will help you gather all locations you visited in France and see which districts, municipalities and regions you visited. It will count them for you and draw nice maps so you can effectivly see where you&apos;ve been.
+        {t("want-to-try")} <Link href={t("register-link")}>{t("register")}</Link> {t("what-is-purpose-res3")}
         </p>
 
-        <h2>What is the ranking on ▤ 36680 ▥?</h2>
+        <h2>{t("what-is-36680")}</h2>
         <p className="mb-4">
-        Visiting 36680 municipalities and find euros in each of them is quite a challenge. This has started as a game for Eurobilltraker users <a href="https://forum.eurobilltracker.com/viewtopic.php?f=34&t=7171">launched by TedX on Eurobilltracker forum</a> in 2005. This game also counts population and surface for all players. The main ranking counts the number of visited municipalities. This is the ranking you see on <b>▤ 36680 ▥</b>.
+        {t("what-is-36680-res1")}
         </p>
         <p className="mb-4">
-          <b>▤ 36680 ▥</b> allows you also to compete with other players on the number of districs, of administrative cental cities and regions.
+        {t("what-is-36680-res2")}
         </p>
         <p className="mb-4">
-          This is fun and challenging! Why don&apos;t you start today.
-        </p>
-
-        <h2>How can I report a bug or ask for a new feature?</h2>
-        <p className="mb-4">
-          Just contact the author (see bellow) and you&apos;ll soon get feedback.
+        {t("what-is-36680-res3")}
         </p>
 
-        <h2>How can I contact you?</h2>
+        <h2>{t("what-is-ranking-36680")}</h2>
         <p className="mb-4">
-        The best way to comment on this app is to do it <Link href="https://forum.eurobilltracker.com/viewtopic.php?t=57328">on the eurobilltraker forum</Link> on the dedicated thread. You may also contact the author directly <Link href="https://alix.guillard.fr/">from his home page</Link>.
+        {t("what-is-ranking-36680-res1")} <Link href={t("launched-by-link")}>{t("launched-by")}</Link>. {t("what-is-ranking-36680-res2")}
+        </p>
+        <p className="mb-4">
+        {t("what-is-ranking-36680-res3")}
+        </p>
+        <p className="mb-4">
+        {t("what-is-ranking-36680-res4")}
+          
+        </p>
+
+        <h2>{t("how-to-report")}</h2>
+        <p className="mb-4">
+        {t("how-to-report-res")}
+        </p>
+
+        <h2>{t("how-to-contact")}</h2>
+        <p className="mb-4">
+        {t("how-to-contact-res1")} <Link href={t("contact-me-link")}>{t("contact-me")}</Link> {t("contact-me-end")} 
+        {t("how-to-contact-res2")} <Link href={t("home-page-link")}>{t("home-page")}</Link>{t("home-page-end")}
+        
         </p>
       </div>
 
