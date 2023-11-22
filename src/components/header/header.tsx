c81c8c9ec53f2ef0406a2b92f9f4c0a6d4e31957
aloxe <github@blog.re>
2023-11-22 23:49:54 +0100

    resize map

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,7 +9,7 @@ export default async function Header({ lang }: {lang: string}) {
 
   return (
     <div className='grid grid-cols-4 gap-4 top-0 p-0 m-0 h-12 bg-sky-200 border-b-2 border-black fixed w-full z-1001'>
-      <div className='col-span-3 p-2 text-black text-xs sm:text-sm md:text-lg font-semibold'>
+      <div className='col-span-3 p-2 text-black text-xs sm:text-sm md:text-lg font-semibold h-[45px]'>
         <Link
           className="pointer-events-auto m-0 no-underline text-black font-bold"
           href={`/${lang}/`}
@@ -23,7 +23,7 @@ export default async function Header({ lang }: {lang: string}) {
           <span className='inline-block text-center'>Eurobilltracker ▤ 36680 ▥</span>
         </Link>
       </div>
-      <div className='mr-2 p-2 text-black'>
+      <div className='mr-2 p-2 text-black h-[45px]'>
         <Login lang={lang} />
       </div>
 		</div>
