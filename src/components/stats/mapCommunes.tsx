7f41800bcabc92e9b41374b5bcdbc23c303b0866
aloxe <github@blog.re>
2023-11-22 09:50:03 +0100

    handle region promisses for map

--- src/components/stats/mapCommunes.tsx
+++ src/components/stats/mapCommunes.tsx
@@ -1,9 +1,10 @@
 'use client'
 import { getUserPolygons } from "@/helpers/dbutils";
 import { MyMapComponent } from "../maps/map";
-import { useTranslation } from '@/i18n'
+import { useTranslation } from '@/i18n/client'
 import { getUserPolygones } from "@/helpers/maputils";
 import { useCallback, useEffect, useState } from "react";
+import Spinner from "../common/spinner";
 
 interface MapCommunesProps {
   lang: string
@@ -14,22 +15,17 @@ interface MapCommunesProps {
 
 function MapCommunes({lang, user, communes, departements}: MapCommunesProps) {
   /* eslint-disable react-hooks/rules-of-hooks */
-  // const { t } = await useTranslation(lang, 'stats')
+  const { t } = useTranslation(lang, 'stats')
   const { username } = user;
   const [mapPolygons, setMapPolygons] = useState<any>(undefined);
   const [ fetching, setFetching ] = useState<boolean>(false);
-
-  // TODO removing BE call and all ref to it
-  // const polygons = await getUserPolygons(user.id.toString());
   
+  // NOTE: This is a use-client component because 
+  // sever can't handle polygone data for even 1 departement
   const handlefetchData = useCallback( async () => {
     setFetching(true);
-
     const communesToDisplay = await getUserPolygones(communes, departements)
-    // savePolygons(user.id, communesToDisplay); // TODO do we still save it?
     setMapPolygons(communesToDisplay)
-    // setDisabled(false);
-
 }, [communes, departements])
 
 
@@ -43,14 +39,15 @@ function MapCommunes({lang, user, communes, departements}: MapCommunesProps) {
     <>
       <div className="bg-white rounded-lg border border-blue-200 text-left text-black p-2 m-2 sm:p-4 sm:m-4">
         <div className="">
-          <h2>{username}
-            {/* {t('usermap', {"username": us ername})} */}
+          <h2>
+            {t('usermap', {"username": username})}
           </h2>
         </div>
         <div className="text-left text-lg font-bold mb-4">
-      <div className="w-full h-90 bg-orange-200 overflow-hidden">
-        {!!mapPolygons && <MyMapComponent departements={departements} dataCommunes={mapPolygons} showDep={true} showCom={true} tiles={"carto"} />}
-      </div>
+          <div className="w-full h-90 overflow-hidden text-center">
+            {!mapPolygons && <Spinner />}
+            {!!mapPolygons && <MyMapComponent departements={departements} dataCommunes={mapPolygons} showDep={true} showCom={true} tiles={"carto"} />}
+          </div>
         </div>
       </div>
     </>
