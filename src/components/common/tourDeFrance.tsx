745c6679565abfb846f6ee679fd033a78b3eda04
aloxe <github@blog.re>
2023-11-30 00:18:19 +0100

    download png image

--- src/components/common/tourDeFrance.tsx
+++ src/components/common/tourDeFrance.tsx
@@ -57,37 +57,93 @@ function TourDeFrance({lang, user, departements, heading, isDashboard}: TourDeFr
 
 [size=85]calculé sur [url=https://ebt.fr.eu.org/]▤ 36680 ▥[/url] avec les communes métropolitaines uniquement[/size]`;
 
+const handleClick = () => {
+  console.log("click");
+  var img = new Image();
+  img.onload = function (){
+      var canvas = document.createElement("canvas");
+      canvas.width = img.naturalWidth;
+      canvas.height = img.naturalHeight;
+      // canvas.width = 334;
+      // canvas.height = 290;
+      console.log("canvas", canvas);
+      
+      var ctxt = canvas.getContext("2d");
+      if (ctxt) {
+        ctxt.fillStyle = "#fff";
+        ctxt.fillRect(0, 0, canvas.width, canvas.height);
+        ctxt.drawImage(img, 0, 0);  
+      }
+      var a = document.createElement("a");
+      a.href = canvas.toDataURL("image/png");
+      a.download = "CARTE_DE_FRANCE.png"
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+  };
+  // #CarteDeFranceSvg
+  var innerSvg = document.querySelector("#CarteDeFranceSvg");
+
+  if (innerSvg) {
+    for(let i = 0; i < innerSvg.children.length; i++) {
+      let regionSvg = innerSvg.children[i];
+      for(let j = 0; j < regionSvg.children.length; j++) {
+        let deptSvg = regionSvg.children[j]
+        if (deptSvg.attributes.length > 0) {
+          deptSvg.setAttribute("stroke-width", "1px")
+          
+          if (visitedDepartements.includes(deptSvg.attributes[1].value)) {
+            deptSvg.setAttribute("fill", "royalblue")
+            deptSvg.setAttribute("stroke", "navy")
+          } else {
+            deptSvg.setAttribute("fill", "aliceblue")
+            deptSvg.setAttribute("stroke", "CornflowerBlue")
+          }
+        }
+      }
+    }
+    var svgText = (new XMLSerializer()).serializeToString(innerSvg);
+    img.src = "data:image/svg+xml;utf8," + encodeURIComponent(svgText);
+  }
+}
+
   return (
-    <>
-      <div className="bg-white rounded-lg border border-blue-200 text-left text-black p-2 m-2 sm:p-4 sm:m-4">
-        <div className="flex justify-between">
-          <h2>
-            {heading}
-          </h2>
+    <div className="bg-white rounded-lg border border-blue-200 text-left text-black p-2 m-2 sm:p-4 sm:m-4">
+      <div className="md:table border-spacing-x-4">
+        <div className="md:basis-1/4 md:table-cell align-bottom">
+          <div className="flex justify-between">
+            <h2>
+              {heading}
+            </h2>
+          </div>
+          <div className="text-left text-md mb-4">
+            <>
+            {visitedRegions.map(region => {
+              return <div key={region.name}>
+                <span className={`block font-thin text-lg mt-2 ${region.deps.filter(d => d.visited).length === 0 ? "opacity-30" : region.deps.filter(d => d.visited).length === region.deps.length ? "opacity-100" : "opacity-60"}`}>{region.name} {region.deps.filter(d => d.visited).length === region.deps.length && <>🏆</>} ({region.deps.filter(d => d.visited).length}/{region.deps.length}) :
+                </span>
+                  {region.deps.map(dep => {
+                    return <span key={dep.code} className={dep.visited ? "font-semibold text-blue-800" : "opacity-30"}> {dep.code}</span>
+                  })}
+              </div>
+            })}
+            </>
+          </div>
         </div>
-        <div className="text-left text-md mb-4">
-          <>
-          {visitedRegions.map(region => {
-            return <div key={region.name}>
-              <span className={`block font-thin text-lg mt-2 ${region.deps.filter(d => d.visited).length === 0 ? "opacity-30" : region.deps.filter(d => d.visited).length === region.deps.length ? "opacity-100" : "opacity-60"}`}>{region.name} {region.deps.filter(d => d.visited).length === region.deps.length && <>🏆</>} ({region.deps.filter(d => d.visited).length}/{region.deps.length}) :
-              </span>
-                {region.deps.map(dep => {
-                  return <span key={dep.code} className={dep.visited ? "font-semibold text-blue-800" : "opacity-30"}> {dep.code}</span>
-                })}
-            </div>
-          })}
-          </>
+        <div className="md:basis-1/4 md:table-cell text-center">
+            <CarteDeFranceSvg visited={visitedDepartements} />
+            {!isDashboard && <></>}
+            {isDashboard && <button type="button" onClick={handleClick} className="btn max-w-min mx-auto m-5 p-0 sm:btn-primary sm:px-4 sm:h-[40px] cursor-pointer text-right">download map</button>}
         </div>
+      </div>
+      <div>
         {!isDashboard && <></>}
         {isDashboard && <>
           {/* /!\ bellow is a client component */}
           <PostToForum lang={lang} message={BBCode} topic={"6255"} />
         </>}
-        <div className="text-left text-md mb-4">
-          <CarteDeFranceSvg visited={visitedDepartements} />
-        </div>
       </div>
-    </>
+    </div>
   )
 }
 
