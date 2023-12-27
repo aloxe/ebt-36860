93db6df760d9a4c0a805fe41d92e949d619d0df6
aloxe <github@blog.re>
2023-12-27 01:14:30 +0100

    submit user data

--- src/app/[lang]/dashboard/admin/userList.tsx
+++ src/app/[lang]/dashboard/admin/userList.tsx
@@ -1,16 +1,24 @@
 'use client'
 import { useAuth } from "@/context/authcontext";
 import Spinner from "@/components/common/spinner";
-import Link from "next/link";
 import moment from "moment";
 import 'moment/min/locales';
 import { isJson } from "@/helpers/strings";
+import { useTranslation } from '@/i18n/client'
+
 
 const  UserList = ({ players }: {players: DbUser[]}) => {
   const { isAdmin } = useAuth()
   moment.locale('en-gb');
- 
-  if (!isAdmin) return <></>
+  const { t } = useTranslation('en', 'dashboard');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log(event.target.my_flag.value + " " + event.target.username.value);
+  }
+
+    if (!isAdmin) return <></>
 
   return (
     <>
@@ -23,7 +31,7 @@ const  UserList = ({ players }: {players: DbUser[]}) => {
           <th className="px-3 md:px-6 py-2 text-center">session</th>
           <th className="px-3 md:px-6 py-2 text-center">geo</th>
           <th className="px-3 md:px-6 py-2 text-center">💶</th>
-          {/* <th className="px-3 md:px-6 py-2 text-center">json</th> */}
+          <th className="px-3 md:px-6 py-2 text-center">sav</th>
         </tr>
       </thead>
       <tbody>
@@ -38,9 +46,9 @@ const  UserList = ({ players }: {players: DbUser[]}) => {
           </td>
           <td>
           <>{p.flag} {p.username}</><br/>
-          <div className="text-center font-thin text-sm">{moment(p.date).format('DD/MM/YYYY HH:mm')}</div>
+          <div className="text-center font-thin text-sm">date{moment(p.date).format('DD/MM/YYYY HH:mm')}</div>
           </td>
-          <td>
+          <td className="text-xs">
             {isJson(p.user) ? JSON.parse(p.user).sessionid : p.user }<br/>
             {isJson(p.user) ? JSON.parse(p.user).date : '' }
           </td>
@@ -48,14 +56,31 @@ const  UserList = ({ players }: {players: DbUser[]}) => {
           {isJson(p.user) && JSON.parse(p.user).my_zip +" "+ JSON.parse(p.user).my_city}<br/>
           {isJson(p.user) && JSON.parse(p.user).my_country}<br/>
           {isJson(p.user) && JSON.parse(p.user).email}
+
           </td>
           <td>
           💶: {isJson(p.user) && JSON.parse(p.user).totalbills}<br/>
           🏆: {isJson(p.user) && JSON.parse(p.user).totalhits}
           </td>
-          {/* <td>
-          {JSON.stringify(p)}
-          </td> */}
+          {<td>
+            <form onSubmit={handleSubmit}>
+            <input type="hidden" value={p.id} name="id" id="id" />
+            <input type="hidden" value={p.username} name="username" id="username" />
+            <input type="hidden" value={isJson(p.user) ? JSON.parse(p.user).sessionid : p.user } name="sessionid" id="sessionid" />
+            <input type="hidden" value={isJson(p.user) && JSON.parse(p.user).email} name="email" id="email" />
+            <input type="hidden" value={isJson(p.user) && JSON.parse(p.user).my_country} name="my_country" id="my_country" />
+            <input type="hidden" value={isJson(p.user) && JSON.parse(p.user).my_city} name="my_city" id="my_city" />
+            <input type="hidden" value={isJson(p.user) && JSON.parse(p.user).my_zip} name="my_zip" id="my_zip" />
+            <input type="hidden" value={p.flag} name="my_flag" id="my_flag" />
+            <input type="hidden" value={isJson(p.user) && JSON.parse(p.user).totalbills} name="totalbills" id="totalbills" />
+            <input type="hidden" value={isJson(p.user) && JSON.parse(p.user).totalhits} name="totalhits" id="totalhits" />
+
+            <button className="btn max-w-min mx-auto m-5 p-0 sm:btn-primary sm:px-4 sm:h-[40px] cursor-pointer" id="save" type="submit">
+              <span className="sm:hidden">💾</span>
+              <span className="hidden sm:inline-block">{t('save')}</span>
+            </button>
+            </form>
+          </td>}
         </tr>
         ))}
       </tbody>
